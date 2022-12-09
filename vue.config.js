bced674fab9ddf9e818202e33edf031f133bbc5f
不求圣剑 <suyulingxm@hotmail.com>
2022-12-09 09:10:36 +0800

    fix: vue cli hotload 不更新的bug

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { VantResolver } = require("unplugin-vue-components/resolvers");
 const ComponentsPlugin = require("unplugin-vue-components/webpack");
 
 module.exports = {
+   devServer: {
+    disableHostCheck: true
+  },
   configureWebpack: {
     plugins: [
       ComponentsPlugin({
